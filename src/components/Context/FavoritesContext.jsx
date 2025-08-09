c0eec1ec3b8c9db7be8c48abde182f303c910495
mlsfiles75@gmail.com <mlsfiles75@gmail.com>
2025-08-08 22:17:28 -0400

    added routers, not usable at the moment, added components, console.log api to debug

---
+++ src/components/Context/FavoritesContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, useState } from "lodash";
+import useLocalStorage from "../Utils/useLocalStorage";
+
+export const FavoritesContext = React.createContext({favorites: []}) 
+addFavorites,
+removeFavorites,
+existingFavorites
+
+
+export default function FavoritesProvider() {
+    const [favorites, setFacorites]= useLocalStorage([])
+
+addFavorites,
+removeFavorites,
+existingFavorites
+
+    function addFavorites() {
+
+    }
+
+    function removeFavorites() {
+
+    }
+
+    function existingFavorites() {
+
+    }
+
+return (
+    <div>
+        <h2>Favorite Recipe</h2>
+    </div>
+)
+
+}
